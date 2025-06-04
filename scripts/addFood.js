4f3d0949bc719258f3da1e33076619f93daa5b79
Joax <joaxdev9@gmail.com>
2025-06-04 06:56:11 -0300

    Added a functional navigation bar so the user will be able to go between both available pages, as well as some styling for them.

--- scripts/addFood.js
+++ scripts/addFood.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import { addFood } from "./dbManager";
+import { addFood } from "./dbManager.js";
 
 const form = document.getElementById("food-adding");
 
@@ -12,12 +12,14 @@ form.addEventListener("select", ()=>{
     // let foodNameValue = document.getElementById("food-name").innerHTML;
 
     food = {
-        foodName : document.getElementById("food-name").innerHTML,
+        foodName : document.getElementById("foodName").innerHTML,
         calories : document.getElementById("calories").innerHTML,
         carbohidrates : document.getElementById("carbohidrates").innerHTML,
         protein : document.getElementById("protein").innerHTML,
         fat : document.getElementById("fat")
     }
+
+    food = [food, foodName];
 
     addFood(food);
 })
